9cf44ac1e489e8eb67bc91d4ff2e3f09d1e2d8ab
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-04 14:09:28 +0700

    use cdk detect resolution size for hide sidebar when use in mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'fullstack-front-end-angular';
+
+  mobileQueryMax: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(
+    //detect resolution
+    changeDetectorRef: ChangeDetectorRef,
+    //match resolution and condition
+    media: MediaMatcher
+  ) {
+    //decare _mobileQueryListener for detect by detectChanges method
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    //decare max width for condition
+    this.mobileQueryMax = media.matchMedia('(max-width: 600px)');
+    //add condition
+    this.mobileQueryMax.addEventListener('change', this._mobileQueryListener);
+  }
+
+  //clear 
+  ngOnDestroy() : void{
+    this.mobileQueryMax.removeEventListener('change', this._mobileQueryListener);
+  }
+
 }
 
