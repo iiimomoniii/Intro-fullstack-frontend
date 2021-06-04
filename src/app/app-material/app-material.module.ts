abc988f3826f27f4bd6c8df1b461ac5e7635abce
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-04 10:05:21 +0700

    prepaire angular material

---
+++ src/app/app-material/app-material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+const materialModules = [
+  MatToolbarModule,
+  MatIconModule
+];
+
+@NgModule({
+    imports: [
+      materialModules,
+    ],
+    exports: [
+      materialModules,
+    ]
+  })
+  export class AppMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppMaterialModule } from '@app/app-material/app-material.module';
+
+import { HeaderComponent } from '@app/components/layouts/header/header.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
