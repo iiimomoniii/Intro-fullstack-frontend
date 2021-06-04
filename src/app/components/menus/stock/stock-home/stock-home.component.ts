b293eb6b46bca06d3b4e52c61a8207d875dc8709
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-04 22:16:56 +0700

    edit stock => datatable
    paginator datatable

--- src/app/components/menus/stock/stock-home/stock-home.component.ts
+++ src/app/components/menus/stock/stock-home/stock-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Product } from '@models/product.model';
@@ -10,7 +11,7 @@ import { Product } from '@models/product.model';
 export class StockHomeComponent implements OnInit {
 
   //Data Objects
-  displayedColumns : string[] = ['image', 'name', 'price','stock', 'action'];
+  displayedColumns: string[] = ['image', 'name', 'price', 'stock', 'action'];
   dataSource = new MatTableDataSource<Product>();
 
   //search 
@@ -18,47 +19,197 @@ export class StockHomeComponent implements OnInit {
 
   //sort
   //bind sort to matSort
-  @ViewChild(MatSort, {static : true}) sort : MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  //paging
+  //bind sort to mat-paginator
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit(): void {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
     this.feedData();
   }
 
   //call datas from fake datas
-  feedData(){
-    
+  feedData() {
+
     const dummy: Product[] = [
-     {
-      name : "Repellat ipsam placeat ex alias sunt. Dolore mollitia rerum a veritatis consectetur hic veniam. Ipsam reprehenderit itaque fugit accusamus culpa libero et qui. Quia ipsum et facere. Quos eveniet itaque sequi cupiditate.",
-      stock : 100,
-      price : 20000,
-      image: "assets/images/products/macbook.jpg"
-     },
-     {
-      name : "macbook",
-      stock : 100,
-      price : 12542,
-      image: "assets/images/products/macbook.jpg"
-     },
-     {
-      name : "macbook",
-      stock : 100,
-      price : 1454,
-      image: "assets/images/products/macbook.jpg"
-     },
-     {
-      name : "macbook",
-      stock : 100,
-      price : 187811,
-      image: "assets/images/products/macbook.jpg"
-     },
-     {
-        name : "macbook",
-        stock : 100,
-        price : 15448,
+      {
+        name: "Repellat ipsam placeat ex alias sunt. Dolore mollitia rerum a veritatis consectetur hic veniam. Ipsam reprehenderit itaque fugit accusamus culpa libero et qui. Quia ipsum et facere. Quos eveniet itaque sequi cupiditate.",
+        stock: 100,
+        price: 20000,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 12542,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 1454,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 187811,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 15448,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "Repellat ipsam placeat ex alias sunt. Dolore mollitia rerum a veritatis consectetur hic veniam. Ipsam reprehenderit itaque fugit accusamus culpa libero et qui. Quia ipsum et facere. Quos eveniet itaque sequi cupiditate.",
+        stock: 100,
+        price: 20000,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 12542,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 1454,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 187811,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 15448,
+        image: "assets/images/products/macbook.jpg"
+      }, {
+        name: "Repellat ipsam placeat ex alias sunt. Dolore mollitia rerum a veritatis consectetur hic veniam. Ipsam reprehenderit itaque fugit accusamus culpa libero et qui. Quia ipsum et facere. Quos eveniet itaque sequi cupiditate.",
+        stock: 100,
+        price: 20000,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 12542,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 1454,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 187811,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 15448,
+        image: "assets/images/products/macbook.jpg"
+      }, {
+        name: "Repellat ipsam placeat ex alias sunt. Dolore mollitia rerum a veritatis consectetur hic veniam. Ipsam reprehenderit itaque fugit accusamus culpa libero et qui. Quia ipsum et facere. Quos eveniet itaque sequi cupiditate.",
+        stock: 100,
+        price: 20000,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 12542,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 1454,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 187811,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 15448,
+        image: "assets/images/products/macbook.jpg"
+      }, {
+        name: "Repellat ipsam placeat ex alias sunt. Dolore mollitia rerum a veritatis consectetur hic veniam. Ipsam reprehenderit itaque fugit accusamus culpa libero et qui. Quia ipsum et facere. Quos eveniet itaque sequi cupiditate.",
+        stock: 100,
+        price: 20000,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 12542,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 1454,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 187811,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 15448,
+        image: "assets/images/products/macbook.jpg"
+      }, {
+        name: "Repellat ipsam placeat ex alias sunt. Dolore mollitia rerum a veritatis consectetur hic veniam. Ipsam reprehenderit itaque fugit accusamus culpa libero et qui. Quia ipsum et facere. Quos eveniet itaque sequi cupiditate.",
+        stock: 100,
+        price: 20000,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 12542,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 1454,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 187811,
+        image: "assets/images/products/macbook.jpg"
+      },
+      {
+        name: "macbook",
+        stock: 100,
+        price: 15448,
         image: "assets/images/products/macbook.jpg"
       },
     ];
@@ -66,7 +217,7 @@ export class StockHomeComponent implements OnInit {
     this.dataSource.data = dummy;
   }
 
-  search(event :any){
+  search(event: any) {
     let fliterValue = '';
     if (event) {
       //tranfer and get value from HTML Input 
@@ -75,7 +226,7 @@ export class StockHomeComponent implements OnInit {
     this.dataSource.filter = fliterValue.trim().toLocaleLowerCase();
   }
 
-  clearSearch(){
+  clearSearch() {
     this.textSearch = '';
     this.search(null);
   }
