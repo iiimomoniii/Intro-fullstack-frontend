afaf2f0bebe4b563f31115292edef05418ccb444
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-04 10:56:01 +0700

    add button for login and logout
    matMenuTriggerFor refer id by # example list

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -3,12 +3,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
 
 const materialModules = [
   MatToolbarModule,
   MatIconModule,
   MatButtonModule,
-  MatBadgeModule
+  MatBadgeModule,
+  MatMenuModule
 ];
 
 @NgModule({
