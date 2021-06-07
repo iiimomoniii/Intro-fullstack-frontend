e18224c29c5a2b6ab84dc51358a2f0c279d4f81f
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-07 13:22:26 +0700

    prepair stock create form

--- src/app/components/menus/stock/stock-create/stock-create.component.ts
+++ src/app/components/menus/stock/stock-create/stock-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { Product } from '@models/product.model';
 @Component({
   selector: 'app-stock-create',
   templateUrl: './stock-create.component.html',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockCreateComponent implements OnInit {
 
+  //pipe "|"" for more 1 type
+  imagePreview : string | ArrayBuffer | null;
+  //file for image
+  file : File;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onPreviewImage(event : Event){
+    const image =  (event.target as HTMLInputElement)?.files?.[0];
+    if (image) {
+      //assign image to file for get name
+      this.file = image;
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+    }
+  }
+
+  onSubmit(productForm : NgForm){
+    //check require field from productForm
+    if(productForm.invalid){
+      return;
+    }
+    //get values from productForm
+    const values = productForm.value;
+    //new product obj
+    let product = new Product();
+    //assign value to obj
+    product.name = values.name;
+    product.price = values.price;
+    product.stock = values.stock;
+    product.image = this.file.name;
+    alert(JSON.stringify(product));
   }
 
 }
