545f48bc42941bccfd3d7aa37b4a7097339f18e5
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-10 11:04:54 +0700

    create and move product obj to product response

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { WebapiService } from '@services/webapi.service';
-import { Product } from '@models/product.model';
+import { ProductResponse } from '@models/product.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +11,8 @@ export class ProductService {
     private webapi: WebapiService
   ) { }
   
-  getProducts(): Observable<Product>{
-    return this.webapi.get<Product>('product');
+  getProducts(): Observable<ProductResponse[]>{
+    return this.webapi.get<ProductResponse[]>('product');
   }
 
 }
