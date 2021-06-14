0d931fb6b425618cc18d776b4a687093a86e7075
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-14 11:26:59 +0700

    create service for spinner by cdk

--- src/app/components/shared/progress/progress.component.ts
+++ src/app/components/shared/progress/progress.component.ts
@@ -1,24 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { AfterViewInit, Component, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { LoadingService } from '@services/loading.service';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.scss']
 })
-export class ProgressComponent implements OnInit {
-  
+export class ProgressComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('templatePortalContent') templatePortalContent: TemplateRef<any>;
+
   @Input() color: ThemePalette;
-  @Input() strokeWidth : number;
-  @Input() diameter : number = 100;
+  @Input() strokeWidth: number;
+  @Input() diameter: number = 100;
   @Input() mode: ProgressSpinnerMode;
   @Input() value: number;
 
-  constructor() { }
+  constructor(
+    private _viewContainerRef: ViewContainerRef,
+    private loadingService: LoadingService) { }
 
   ngOnInit(): void {
 
+  }
+
+  ngAfterViewInit() {
+    this.loadingService.attach(this.templatePortalContent, this._viewContainerRef);
   }
 
 }
---
+++ src/services/loading.service.ts
@@ -0,0 +1,38 @@
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { TemplateRef } from '@angular/core';
+import { Injectable, ViewContainerRef } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+
+  private overlayRef: OverlayRef = this.createOverlay();
+  private templatePortal: TemplatePortal<any>;
+
+  constructor(private overlay : Overlay) { }
+
+  private createOverlay() : OverlayRef {
+    return this.overlay.create({
+      hasBackdrop : true,
+      positionStrategy: this.overlay.position()
+      .global()
+      .centerVertically()
+      .centerHorizontally()
+    });
+  }
+
+  private showSpinner(){
+    this.overlayRef.attach(this.templatePortal);
+  }
+
+  private hideSpinner(){
+    this.overlayRef.detach();
+  }
+
+  attach(templatePortalContent : TemplateRef<any>, viewContainerRef : ViewContainerRef) {
+    this.templatePortal = new TemplatePortal(templatePortalContent, viewContainerRef);
+  }
+}
