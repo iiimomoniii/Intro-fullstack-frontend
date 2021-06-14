9b2f31dd4e71cadfe6bfc5ad6724cfa572a55868
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-14 15:49:03 +0700

    create path for stock when empty and doesn't match

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,13 @@ import { StockCreateComponent } from '@menus/stock/stock-create/stock-create.com
 import { StockEditComponent } from '@menus/stock/stock-edit/stock-edit.component';
 
 const routes: Routes = [
+  
+  { path: '', redirectTo: 'stock', pathMatch: 'full' },
   { path: 'stock', component: StockHomeComponent },
   { path: 'stock/create', component: StockCreateComponent },
   { path: 'stock/edit/:id', component: StockEditComponent },
+  { path: '**', redirectTo: 'stock'}
+
 ];
 
 @NgModule({
